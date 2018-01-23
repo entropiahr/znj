f16c08bbdef1d15697c7879ca0161db5afb3340e
Ivo Kosir <ivo.kosir93@gmail.com>
2018-01-23 21:18:35 +0100

    Create normalizer

---
+++ compiler.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+from llvmlite import ir
+
+root_ast = {
+    "type": "block",
+    "body": [
+        {
+            "type": "def",
+            "name": "val_x",
+            "args": [],
+            "body": {
+                "type": "call",
+                "name": "add",
+                "args": [
+                    {"type": "integer", "value": 1},
+                    {"type": "integer", "value": 1}
+                ]
+            }
+        },
+        {
+            "type": "def",
+            "name": "lambda",
+            "args": ["number"],
+            "body": {
+                "type": "call",
+                "name": "add",
+                "args": [
+                    {"type": "call", "name": "val_x", "args": []},
+                    {"type": "call", "name": "number", "args": []}
+                ]
+            }
+        },
+        {
+            "type": "call",
+            "name": "lambda",
+            "args": [
+                {"type": "integer", "value": 20}
+            ]
+        }
+    ]
+}
+
+int_type = ir.IntType(32)
+
+module = ir.Module()
+module.triple = "x86_64-unknown-linux-gnu"
+
+# TODO: maybe have two cycles
+# first cycle: flatten (no context args to functions)
+# second cycle: actual codegen
+
+class Expression:
+    def __init__(self, context, used_context, call):
+        self.context = context
+        self.used_context = used_context
+        self.call = call
+
+    @classmethod
+    def integer(cls, context, value):
+        return cls(context, dict(), lambda builder, args: ir.Constant(int_type, value))
+
+
+class Context:
+    def __init__(self, parent, expressions):
+        self.expressions = expressions
+        self.parent = parent
+
+    @classmethod
+    def root(cls, expressions):
+        return cls(None, expressions)
+
+    def copy(self):
+        parent = self.parent.copy() if self.parent is not None else None
+        expressions = self.expressions.copy()
+        return self.__class__(parent, expressions)
+
+    def add(self, name, call):
+        new = self.copy()
+        new.expressions.update({name: call})
+        return new
+
+    def find(self, name):
+        expression = self.expressions.get(name)
+        if expression is not None: return expression
+        if self.parent is None: return None
+        return self.parent.find(name)
+
+def create_def(ast, context):
+    args = ast["args"]
+    name = ast["name"]
+    body = ast["body"]
+    if args or name == "main":
+        context_args = [(name, call) for name, call in context.expressions.items() if name == "val_x"]
+        context_arg_names = [name for name, _ in context_args]
+        arg_names = context_arg_names + ast["args"]
+
+        base_arg_types = [int_type for _ in arg_names]
+        base_args = tuple(Argument())
+        function_type = ir.FunctionType(int_type, arg_types)
+
+        function = ir.Function(module, type, ast["name"])
+
+        arg_calls = map(lambda arg: lambda _builder, _args: arg, function.args)
+        args = dict(zip(arg_names, arg_calls))
+
+        child_context = Context(context, args)
+        child_expression = create_expression(body, child_context)
+        used_context = child_expression.used_context
+
+        block = function.append_basic_block("entry")
+        child_builder = ir.IRBuilder(block)
+        child_builder.ret(child_expression.call(child_builder, []))
+
+        def call(builder, args):
+            context_arg_values = [call(builder, []) for _, call in context_args]
+            return builder.call(function, context_arg_values + args)
+    else:
+        child_expression = create_expression(body, context)
+        used_context = child_expression.used_context
+        call = child_expression.call
+
+    if used_context: print(used_context)
+    context = context.add(name, call)
+    return Expression(context, used_context, call)
+
+def create_block(ast, context):
+    body = ast["body"]
+    if not body: raise ValueError("body mustn't be empty!")
+
+    child_context = context
+    for child in body:
+        child_expression = create_expression(child, child_context)
+        child_context = child_expression.context
+        last_expression = child_expression
+
+    last_expression.context = context
+    return last_expression
+
+def create_integer(ast, context):
+    return Expression.integer(context, ast["value"])
+
+def create_call(ast, context):
+    this_call = context.find(ast["name"])
+    arg_expressions = [create_expression(arg, context) for arg in ast["args"]]
+
+    used_context = {ast["name"]: this_call}
+    for arg_expression in arg_expressions:
+        used_context.update(arg_expression.used_context)
+
+    arg_calls = [arg_expression.call for arg_expression in arg_expressions]
+    def call(builder, args):
+        args = [arg_call(builder, []) for arg_call in arg_calls]
+        return this_call(builder, args)
+
+    return Expression(context, used_context, call)
+
+def create_expression(ast, context):
+    t = ast["type"]
+
+    if t == "def":
+        return create_def(ast, context)
+    elif t == "integer":
+        return create_integer(ast, context)
+    elif t == "call":
+        return create_call(ast, context)
+    elif t == "block":
+        return create_block(ast, context)
+    else:
+        raise ValueError("Wrong type: " + str(ast["type"]))
+
+
+root_context = Context.root({
+    "add": lambda builder, args: builder.add(args[0], args[1])
+})
+
+main = ir.Function(module, ir.FunctionType(int_type, ()), name="main")
+block = main.append_basic_block("entry")
+builder = ir.IRBuilder(block)
+
+root_expression = create_expression(root_ast, root_context)
+result = root_expression.call(builder, [])
+builder.ret(result)
+
+def unused():
+    my_add = ir.Function(module, ir.FunctionType(int_type, (int_type, int_type)), name="my_add")
+
+    block = func.append_basic_block(name="entry")
+    builder = ir.IRBuilder(block)
+    a, b, c = func.args
+    result = builder.add(a, b, name="res")
+    result = builder.add(result, c, name="res")
+    builder.ret(result)
+
+    main = ir.Function(module, ir.FunctionType(int_type, ()), name="main")
+    block = main.append_basic_block()
+    builder.position_at_start(block)
+    first = builder.alloca(int_type, name="first")
+    builder.store(ir.Constant(int_type, 11), first)
+    func_ptr = builder.alloca(my_add.type.as_pointer(), name="func_ptr")
+    builder.store(my_add, func_ptr)
+    result = builder.call(func, (ir.Constant(int_type, 23), ir.Constant(int_type, 23), ir.Constant(int_type, 2)))
+    builder.ret(result)
+
+print("\n\n=======================================\n\n")
+print(module)
+with open("test.ll", "w") as output:
+    output.write(str(module))
---
+++ normalizer.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+root_ast = {
+    "type": "block",
+    "body": [
+        {
+            "type": "def",
+            "name": "val_x",
+            "args": [],
+            "body": {
+                "type": "call",
+                "name": "add",
+                "args": [
+                    {"type": "integer", "value": 1},
+                    {"type": "integer", "value": 1}
+                ]
+            }
+        },
+        {
+            "type": "def",
+            "name": "lambda",
+            "args": ["number"],
+            "body": {
+                "type": "call",
+                "name": "add",
+                "args": [
+                    {"type": "call", "name": "val_x", "args": []},
+                    {"type": "call", "name": "number", "args": []}
+                ]
+            }
+        },
+        {
+            "type": "call",
+            "name": "lambda",
+            "args": [
+                {"type": "integer", "value": 20}
+            ]
+        }
+    ]
+}
+
+
+class ScopeName:
+    def __init__(self, type, name):
+        self.type = type
+        self.name = name
+        self.env = []
+
+    def copy(self):
+        new = self.__class__(self.type, self.name)
+        new.env = self.env.copy()
+        return new
+
+    def add_env(self, env):
+        self.env = [{"type": "call", "name": env_arg, "args": []} for env_arg in env]
+
+
+class Scope:
+    def __init__(self, parent=None):
+        if parent is None:
+            self.snames = []
+            return
+
+        self.snames = [sname for sname in parent.copy().snames if sname.type == "fn"]
+
+    def copy(self):
+        new = self.__class__()
+        new.snames = [sname.copy() for sname in self.snames]
+        return new
+
+    def add_fns(self, *snames):
+        new = self.copy()
+        new.snames.extend([ScopeName("fn", sname) for sname in snames])
+        return new
+
+    def add_fn_env(self, name, env):
+        new = self.copy()
+        for sname in new.snames:
+            if sname.name == name:
+                sname.add_env(env)
+        return new
+
+    def add_names(self, *snames):
+        new = self.copy()
+        new.snames.extend([ScopeName("name", sname) for sname in snames])
+        return new
+
+    def find(self, name):
+        res = [sname for sname in self.snames if sname.name == name]
+        if res: return res[-1]
+        return None
+
+
+def normalize_def_fn(ast, scope):
+    scope = scope.add_fns(ast["name"])
+
+    child_scope = Scope(scope).add_names(*ast["args"])
+    body_ast, _, used = normalize_expression(ast["body"], child_scope)
+
+    scope = scope.add_fn_env(ast["name"], used)
+    ast["env"] = used
+
+    return (ast, scope, [])
+
+def normalize_def_name(ast, scope):
+    body, _, used = normalize_expression(ast["body"], scope)
+
+    ast = {
+        "type": "def_name",
+        "name": ast["name"],
+        "body": body
+    }
+    scope = scope.add_names(ast["name"])
+    return (ast, scope, used)
+
+def normalize_def(ast, scope):
+    if ast["args"]: return normalize_def_fn(ast, scope)
+    else: return normalize_def_name(ast, scope)
+
+def normalize_integer(ast, scope):
+    return (ast, scope, [])
+
+def normalize_call(ast, scope):
+    scope_name = scope.find(ast["name"])
+    if scope_name:
+        used = []
+        env = scope_name.env
+    else:
+        used = [ast["name"]]
+        env = None
+
+    args = []
+    for arg in ast["args"]:
+        arg, _, arg_used = normalize_expression(arg, scope)
+        args.append(arg)
+        used.extend(arg_used)
+    ast["args"] = args
+
+    if env:
+        env_args = []
+        for env_arg in env:
+            env_arg, _, env_arg_used = normalize_expression(env_arg, scope)
+            env_args.append(env_arg)
+            used.extend(env_arg_used)
+        ast["env"] = env_args
+
+    return (ast, scope, used)
+
+def normalize_block(ast, scope):
+    body = []
+    used = []
+    child_scope = scope
+    for child in ast["body"]:
+        child, child_scope, child_used = normalize_expression(child, child_scope)
+        body.append(child)
+        used.extend(child_used)
+
+    ast["body"] = body
+    return (ast, scope, used)
+
+def normalize_expression(ast, scope):
+    if ast["type"] == "def":
+        return normalize_def(ast, scope)
+    elif ast["type"] == "integer":
+        return normalize_integer(ast, scope)
+    elif ast["type"] == "call":
+        return normalize_call(ast, scope)
+    elif ast["type"] == "block":
+        return normalize_block(ast, scope)
+    else:
+        raise ValueError("Wrong type: " + str(ast["type"]))
+
+def normalize_root_ast(ast):
+    scope = Scope().add_fns("add")
+    ast, _, used = normalize_expression(ast, scope)
+
+    if used:
+        raise ValueError("Can't find names: " + ", ".join(used))
+
+    return ast
+
+
+import json
+
+print(json.dumps(normalize_root_ast(root_ast), indent=4))
